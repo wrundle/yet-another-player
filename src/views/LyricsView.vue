6b9198bcad3ecc2338c463a1793015102616c2be
wrundle <Leptus365@gmail.com>
2023-06-17 17:35:25 +0700

    style: 🎨 Slight refactoring 2

--- src/views/LyricsView.vue
+++ src/views/LyricsView.vue
@@ -1,5 +1,4 @@
 <script setup>
-// @ is an alias to /src
 import TheRightSidebar from '@/components/TheRightSidebar.vue';
 import TheLeftSidebar from '@/components/TheLeftSidebar.vue';
 import TheSongLyrics from '@/components/TheSongLyrics.vue';
@@ -8,17 +7,16 @@ import LoadingMsg from '@/components/TheLoadingMsg.vue';
 import TheTopBar from '@/components/TheTopBar.vue';
 </script>
 
-
 <template>
-	<div class="
-		flex flex-col min-h-screen max-h-screen overflow-hidden
-		bg-white dark:bg-stone-900 dark:text-white
-	">
-
+	<div
+		class="
+			flex flex-col min-h-screen max-h-screen overflow-hidden
+			bg-white dark:bg-stone-900 dark:text-white
+		"
+	>
 		<TheTopBar />
 
 		<div class="flex-grow flex flex-row max-h-full overflow-hidden">
-
 			<TheLeftSidebar />
 
 			<Suspense>
@@ -27,14 +25,8 @@ import TheTopBar from '@/components/TheTopBar.vue';
 			</Suspense>
 
 			<TheRightSidebar />
-
 		</div>
 
 		<TheBottomBar />
-
 	</div>
 </template>
-
-
-<style scoped>
-</style>
--- src/views/TheaterView.vue
+++ src/views/TheaterView.vue
@@ -1,5 +1,4 @@
 <script setup>
-// @ is an alias to /src
 import TheRightSidebar from '@/components/TheRightSidebar.vue';
 import TheLeftSidebar from '@/components/TheLeftSidebar.vue';
 import TheBottomBar from '@/components/TheBottomBar.vue';
@@ -7,21 +6,15 @@ import LoadingMsg from '@/components/TheLoadingMsg.vue';
 import TheTopBar from '@/components/TheTopBar.vue';
 </script>
 
-
 <template>
-	<div class="
-		flex flex-col min-h-screen max-h-screen overflow-hidden
-		bg-white dark:bg-stone-900 dark:text-white
-	">
-
-
+	<div
+		class="
+			flex flex-col min-h-screen max-h-screen overflow-hidden
+			bg-white dark:bg-stone-900 dark:text-white
+		"
+	>
 		<div class="flex-grow flex flex-row max-h-full overflow-hidden"></div>
 
 		<TheBottomBar />
-
 	</div>
 </template>
-
-
-<style scoped>
-</style>
